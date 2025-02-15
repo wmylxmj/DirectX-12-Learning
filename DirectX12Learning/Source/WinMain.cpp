eac94ced3dae71618b2816faff6f075d849ae9aa
wmylxmj <wmylxmj@gmail.com>
2025-02-15 16:44:59 +0800

    添加项目文件。

---
+++ DirectX12Learning/Source/WinMain.cpp
@@ -0,0 +1,12 @@
+#include "Windows.h"
+#include "WindowsX.h"
+
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+) {
+	OutputDebugStringA("Hello World!\n");
+	return 0;
+}
