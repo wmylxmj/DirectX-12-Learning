5360a510a51e95419cebec3639ccf687f0f08e7c
wmylxmj <wmylxmj@gmail.com>
2025-02-18 23:24:13 +0800

    更新

--- DirectX12Learning/Source/ModelLoader.cpp
+++ DirectX12Learning/Source/ModelLoader.cpp
@@ -1,1 +1,24 @@
 #include "ModelLoader.h"
+#include <codecvt>
+
+bool AssimpImportModel(const std::string& pFile, Model& model) {
+
+	Assimp::Importer importer;
+
+	unsigned int pFlags = aiProcess_Triangulate |
+						 aiProcess_JoinIdenticalVertices;
+
+	const aiScene* scene = importer.ReadFile(pFile, pFlags);
+
+	if (scene == nullptr) {
+		MessageBox(0, L"无法导入模型文件", L"模型文件导入失败", 0);
+		return false;
+	}
+
+	return AssimpSceneProcessing(scene, model);
+}
+
+bool AssimpSceneProcessing(const aiScene* scene, Model& model) {
+	return true;
+}
+	
--- DirectX12Learning/Source/ModelLoader.h
+++ DirectX12Learning/Source/ModelLoader.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "Windows.h"
+
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
+
+#include "Model.h"
+
+static bool AssimpSceneProcessing(const aiScene* scene, Model& model);
