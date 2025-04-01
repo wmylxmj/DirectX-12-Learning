5566f12e263949d6bf98dfce252e58fd308ec6c4
wmylxmj <1397322329@qq.com>
2025-04-01 22:50:55 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.cpp
+++ DirectX12Learning/Source/Core/CommandQueue.cpp
@@ -1,1 +1,12 @@
 #include "CommandQueue.h"
+
+CommandQueue::CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, D3D12_COMMAND_LIST_TYPE commandListType) : m_kCommandListType(commandListType)
+{
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Type = commandListType;
+	queueDesc.NodeMask = 1;
+	CHECK_HRESULT(pDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_pCommandQueue)));
+
+	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
+	m_fenceValue = 0;
+}
--- DirectX12Learning/Source/Core/CommandQueue.h
+++ DirectX12Learning/Source/Core/CommandQueue.h
@@ -7,5 +7,12 @@ class CommandQueue
 {
 private:
 	Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_pCommandQueue;
+
+	Microsoft::WRL::ComPtr<ID3D12Fence> m_pFence;
+	uint64_t m_fenceValue;
+
+public:
 	const D3D12_COMMAND_LIST_TYPE m_kCommandListType;
+
+	CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, D3D12_COMMAND_LIST_TYPE commandListType);
 };
