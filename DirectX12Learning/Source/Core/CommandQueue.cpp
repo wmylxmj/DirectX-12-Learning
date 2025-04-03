db5e8a9085e74d3bf3ed91430ad8126417fa7d31
wmylxmj <1397322329@qq.com>
2025-04-03 21:47:15 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.cpp
+++ DirectX12Learning/Source/Core/CommandQueue.cpp
@@ -1,6 +1,9 @@
 ﻿#include "CommandQueue.h"
 
-CommandQueue::CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, D3D12_COMMAND_LIST_TYPE commandListType) : m_kCommandListType(commandListType)
+CommandQueue::CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, D3D12_COMMAND_LIST_TYPE commandListType) :
+	m_kCommandListType(commandListType),
+	m_fenceValue(0),
+	m_completedFenceValueCache(0)
 {
 	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
 	queueDesc.Type = commandListType;
@@ -8,14 +11,13 @@ CommandQueue::CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, D3D12_C
 	CHECK_HRESULT(pDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_pCommandQueue)));
 
 	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
-	m_fenceValue = 0;
 }
 
 bool CommandQueue::IsFenceComplete(uint64_t fenceValue)
 {
 	// 避免查询过于频繁
-	if (fenceValue > m_completedFenceValue) {
-		m_completedFenceValue = std::max(m_completedFenceValue, m_pFence->GetCompletedValue());
+	if (fenceValue > m_completedFenceValueCache) {
+		m_completedFenceValueCache = std::max(m_completedFenceValueCache, m_pFence->GetCompletedValue());
 	}
-	return fenceValue <= m_completedFenceValue;
+	return fenceValue <= m_completedFenceValueCache;
 }
--- DirectX12Learning/Source/Core/CommandQueue.h
+++ DirectX12Learning/Source/Core/CommandQueue.h
@@ -14,7 +14,7 @@ private:
 	std::mutex m_fenceMutex;
 	Microsoft::WRL::ComPtr<ID3D12Fence> m_pFence;
 	uint64_t m_fenceValue;
-	uint64_t m_completedFenceValue;
+	uint64_t m_completedFenceValueCache;
 
 public:
 	CommandQueue(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, D3D12_COMMAND_LIST_TYPE commandListType);
