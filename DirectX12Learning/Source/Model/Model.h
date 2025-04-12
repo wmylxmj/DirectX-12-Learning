e9d220c4f01f683afc9264275cacbb24d0347806
wmylxmj <1397322329@qq.com>
2025-04-12 16:36:01 +0800

    更新模型

--- DirectX12Learning/Source/Model/Model.h
+++ DirectX12Learning/Source/Model/Model.h
@@ -1,17 +1,23 @@
 ﻿#pragma once
 
 /*
-    这里定义 3D 模型
+	这里定义 3D 模型
 */
 
+#include "assimp/scene.h"
 #include "DirectXMath.h"
 
-typedef struct {
-    DirectX::XMFLOAT3 position;
-} Vertex;
+#define NUM_BONES_PER_VERTEX 4
 
+#define INVALID_PARENT 0XFFFFFFFF
 
 class Model {
+public:
 
+	typedef struct {
+		DirectX::XMFLOAT3 position;
+		DirectX::XMFLOAT3 normal;
+	} Vertex;
 
+protected:
 };
