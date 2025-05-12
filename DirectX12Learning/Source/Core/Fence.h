4661a631b9e9ba64414621a9f269cb1157998ce6
wmylxmj <1397322329@qq.com>
2025-05-12 19:58:11 +0800

    更新围栏

--- DirectX12Learning/Source/Core/Fence.h
+++ DirectX12Learning/Source/Core/Fence.h
@@ -2,7 +2,13 @@
 
 #include "PrecompiledHeader.h"
 
+#include <mutex>
+
 class Fence
 {
 private:
+	std::mutex m_fenceMutex;
+	Microsoft::WRL::ComPtr<ID3D12Fence> m_pFence;
+	uint64_t m_fenceValue;
+	uint64_t m_completedFenceValueCache;
 };
