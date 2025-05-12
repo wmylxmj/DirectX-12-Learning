8f47702bacd3dd1a27fe0be1f8db1dbef47f1f06
wmylxmj <1397322329@qq.com>
2025-05-12 20:14:35 +0800

    修复围栏

--- DirectX12Learning/Source/Core/Fence.cpp
+++ DirectX12Learning/Source/Core/Fence.cpp
@@ -4,5 +4,5 @@ Fence::Fence(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, uint64_t initialValue
 	m_fenceValue(initialValue),
 	m_completedFenceValueCache(initialValue)
 {
-	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
+	CHECK_HRESULT(pDevice->CreateFence(initialValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
 }
