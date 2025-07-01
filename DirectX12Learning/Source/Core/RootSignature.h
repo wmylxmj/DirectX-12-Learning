b8c6194cf9931081e3f8ebaeef59b08e3f41e75d
wmylxmj <1397322329@qq.com>
2025-07-01 20:26:16 +0800

    更新根签名

--- DirectX12Learning/Source/Core/RootSignature.h
+++ DirectX12Learning/Source/Core/RootSignature.h
@@ -2,10 +2,6 @@
 
 #include "PrecompiledHeader.h"
 
-struct DescriptorRange : public CD3DX12_DESCRIPTOR_RANGE1
-{
-};
-
-struct RootParameter : public CD3DX12_ROOT_PARAMETER
+struct DescriptorRange : public CD3DX12_DESCRIPTOR_RANGE
 {
 };
