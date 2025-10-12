e8ab2bef6059b7916ecc8da9e1830dabd41d2dd9
wmylxmj <1397322329@qq.com>
2025-10-12 16:27:09 +0800

    更新描述符堆

--- DirectX12Learning/Source/Core/DescriptorHeap.h
+++ DirectX12Learning/Source/Core/DescriptorHeap.h
@@ -4,8 +4,8 @@
 
 class DescriptorHandle
 {
-	public::
-		DescriptorHandle();
+public:
+	DescriptorHandle();
 	DescriptorHandle(D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorHandle, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorHandle);
 
 	void operator +=(int offsetScaledByDescriptorSize);
