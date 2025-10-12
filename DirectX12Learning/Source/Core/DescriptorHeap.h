f83cb970d389c10cc160de22e9f4047ea01fbb1a
wmylxmj <1397322329@qq.com>
2025-10-12 14:55:07 +0800

    更新描述符堆

--- DirectX12Learning/Source/Core/DescriptorHeap.h
+++ DirectX12Learning/Source/Core/DescriptorHeap.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include "PrecompiledHeader.h"
+
 class DescriptorHandle
 {
 public:
+
+private:
+	D3D12_CPU_DESCRIPTOR_HANDLE m_cpuDescriptorHandle;
 };
 
 class DescriptorHeap
 {
 public:
+
+private:
 };
