9c04b24b8954c9d26a9fe8bba824f2cf12ed8658
wmylxmj <1397322329@qq.com>
2025-10-20 16:47:01 +0800

    更新描述符堆

--- DirectX12Learning/Source/Core/DescriptorHeap.h
+++ DirectX12Learning/Source/Core/DescriptorHeap.h
@@ -21,5 +21,14 @@ class DescriptorHeap
 public:
 
 private:
+	const D3D12_DESCRIPTOR_HEAP_TYPE m_kDescriptorHeapType;
+	const uint32_t m_kNumDescriptors;
+	const uint32_t m_kDescriptorSize;
+
+	std::unordered_map<uint64_t, uint64_t> m_pendingFences;
+
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_pDescriptorHeap;
+
+private:
 	DescriptorHandle m_startDescriptorHandle;
 };
