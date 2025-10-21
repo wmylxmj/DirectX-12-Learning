21c623137b53bf2f1f245d85613d9ae14cc2692f
wmylxmj <1397322329@qq.com>
2025-10-21 15:47:18 +0800

    更新动态描述符堆

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.h
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.h
@@ -8,6 +8,26 @@
 #include <vector>
 #include <queue>
 
+;
+
+class DescriptorHeapManager
+{
+public:
+
+private:
+	const uint32_t m_kNumDescriptorsPerHeap;
+	const D3D12_DESCRIPTOR_HEAP_TYPE m_kDescriptorHeapType;
+
+	std::mutex m_mutex;
+
+	// 记录每个命令队列的Fence对象
+	std::unordered_map<uint64_t, std::unique_ptr<Fence>> m_fenceMap;
+
+	std::vector<std::unique_ptr<DescriptorHeap>> m_descriptorHeapPool;
+	std::queue<DescriptorHeap*> m_retiredDescriptorHeaps;
+	std::queue<DescriptorHeap*> m_availableDescriptorHeaps;
+};
+
 class DynamicDescriptorHeap
 {
 };
