c695a3ec3ba34c64813e81fe69b0ebeda7ed2e9a
wmylxmj <1397322329@qq.com>
2025-10-22 09:25:48 +0800

    更新根签名

--- DirectX12Learning/Source/Core/RootSignature.h
+++ DirectX12Learning/Source/Core/RootSignature.h
@@ -22,5 +22,9 @@ public:
 protected:
 	UINT m_numParameters;
 	UINT m_numStaticSamplers;
+
+	std::unique_ptr<RootParameter[]> m_parameters;
+	std::unique_ptr<CD3DX12_STATIC_SAMPLER_DESC[]> m_staticSamplers;
+
 	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_rootSignature;
 };
