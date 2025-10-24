cb1f422cb3c42117b7f2175b92ebb2ab6aad5256
wmylxmj <1397322329@qq.com>
2025-10-24 10:03:52 +0800

    更新动态描述符堆

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
@@ -6,5 +6,27 @@ DescriptorHeapManager::DescriptorHeapManager(D3D12_DESCRIPTOR_HEAP_TYPE descript
 
 DescriptorHeap* DescriptorHeapManager::RequestDescriptorHeap(Microsoft::WRL::ComPtr<ID3D12Device> pDevice)
 {
+	std::lock_guard<std::mutex> lock(m_mutex);
+
+	while (!m_retiredDescriptorHeaps.empty()) {
+		DescriptorHeap* pDescriptorHeap = m_retiredDescriptorHeaps.front();
+
+		// 检测围栏是否已完成
+		bool fencesCompleted = true;
+		for (auto& pendingFence : pDescriptorHeap->m_pendingFences) {
+			if (!m_fenceMap[pendingFence.first]->IsFenceValueCompleted(pendingFence.second)) {
+				fencesCompleted = false;
+				break;
+			}
+		}
+
+		if (fencesCompleted) {
+			m_availableDescriptorHeaps.push(m_retiredDescriptorHeaps.front());
+			m_retiredDescriptorHeaps.pop();
+		}
+		else {
+			break;
+		}
+	}
 	return nullptr;
 }
