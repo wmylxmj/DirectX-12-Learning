497eff3f4b2727896bdc78a793860b9c6aacef48
wmylxmj <1397322329@qq.com>
2025-10-24 15:58:02 +0800

    更新相机

--- DirectX12Learning/Source/Core/Camera.cpp
+++ DirectX12Learning/Source/Core/Camera.cpp
@@ -4,4 +4,13 @@ Camera::Camera()
 {
 	// 设置相机初始参数
 	m_eyePosition = DirectX::XMFLOAT3(0.0f, 0.0f, -5.0f);
+
+	m_forwardDirection = DirectX::XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+	m_upDirection = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	m_rightDirection = DirectX::XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
+
+	m_fovY = DirectX::XM_PIDIV4;
+	m_aspect = 16.0f / 9.0f;
+	m_zNear = 0.1f;
+	m_zFar = 1000.0f;
 }
