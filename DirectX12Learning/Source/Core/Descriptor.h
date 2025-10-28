5fa56a8a0ac2f5bd5b77139a53410737f57a9ca1
wmylxmj <1397322329@qq.com>
2025-10-28 20:51:39 +0800

    更新描述符

--- DirectX12Learning/Source/Core/Descriptor.h
+++ DirectX12Learning/Source/Core/Descriptor.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "PrecompiledHeader.h"
+#include <unordered_map>
+
+class DescriptorHandle
+{
+public:
+	DescriptorHandle();
+	DescriptorHandle(D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorHandle, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorHandle);
+
+	void operator +=(int offsetScaledByDescriptorSize);
+	DescriptorHandle operator +(int offsetScaledByDescriptorSize) const;
+
+private:
+	D3D12_CPU_DESCRIPTOR_HANDLE m_cpuDescriptorHandle;
+	D3D12_GPU_DESCRIPTOR_HANDLE m_gpuDescriptorHandle;
+};
