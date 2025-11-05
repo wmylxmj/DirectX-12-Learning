b8d7207a252274062b3a402c025c42d22abde030
wmylxmj <1397322329@qq.com>
2025-11-05 15:33:58 +0800

    更新命令列表池

--- DirectX12Learning/Source/Core/CommandListPool.h
+++ DirectX12Learning/Source/Core/CommandListPool.h
@@ -6,14 +6,14 @@
 
 class CommandListPool
 {
-private:
-	const D3D12_COMMAND_LIST_TYPE m_kCommandListType;
-	std::queue<Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList>> m_availableCommandLists;
-	std::mutex m_poolMutex;
-
 public:
 	CommandListPool(D3D12_COMMAND_LIST_TYPE commandListType);
 
 	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> RequestCommandList(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, Microsoft::WRL::ComPtr<ID3D12CommandAllocator> pCommandAllocator);
 	void DiscardCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList);
+
+private:
+	const D3D12_COMMAND_LIST_TYPE m_kCommandListType;
+	std::queue<Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList>> m_availableCommandLists;
+	std::mutex m_poolMutex;
 };
