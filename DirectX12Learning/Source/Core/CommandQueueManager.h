e3e992ec5e673b1f9f6734fd85988de147c59a63
wmylxmj <1397322329@qq.com>
2025-11-06 15:06:27 +0800

    更新命令队列管理器

--- DirectX12Learning/Source/Core/CommandQueueManager.h
+++ DirectX12Learning/Source/Core/CommandQueueManager.h
@@ -6,11 +6,5 @@
 class CommandQueueManager
 {
 public:
-	CommandQueueManager(Microsoft::WRL::ComPtr<ID3D12Device> pDevice);
-	~CommandQueueManager();
-
-	void ExecuteCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList);
-	void ExecuteCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList, const CommandQueue& queue);
-	void ExecuteCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList, uint64_t& fenceValue, const CommandQueue& queue);
-	void ExecuteCommandList()
-}
+	CommandQueueManager(ID3D12Device* pDevice);
+};
