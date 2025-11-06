b4cce07ae5e08e3e8e3b79a71ba60b0754c26621
wmylxmj <1397322329@qq.com>
2025-11-06 15:04:59 +0800

    GENGX

--- DirectX12Learning/Source/Core/CommandQueueManager.h
+++ DirectX12Learning/Source/Core/CommandQueueManager.h
@@ -2,3 +2,15 @@
 
 #include "PrecompiledHeader.h"
 #include "CommandQueue.h"
+
+class CommandQueueManager
+{
+public:
+	CommandQueueManager(Microsoft::WRL::ComPtr<ID3D12Device> pDevice);
+	~CommandQueueManager();
+
+	void ExecuteCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList);
+	void ExecuteCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList, const CommandQueue& queue);
+	void ExecuteCommandList(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> pCommandList, uint64_t& fenceValue, const CommandQueue& queue);
+	void ExecuteCommandList()
+}
