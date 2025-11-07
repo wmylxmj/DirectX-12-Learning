1d09232c586b69832f9a904d7c59bb5105822e0d
wmylxmj <1397322329@qq.com>
2025-11-07 10:00:25 +0800

    GX

--- DirectX12Learning/Source/Core/CommandAllocatorPool.cpp
+++ DirectX12Learning/Source/Core/CommandAllocatorPool.cpp
@@ -13,7 +13,7 @@ ID3D12CommandAllocator* CommandAllocatorPool::RequestCommandAllocator(uint64_t c
 	ID3D12CommandAllocator* pAllocator = nullptr;
 
 	// 在已归还的分配器中查看是否有空闲的分配器
-	while (!m_retiredCommandAllocators.empty()) {
+	{
 		std::pair<uint64_t, ID3D12CommandAllocator*>& allocatorPair = m_retiredCommandAllocators.front();
 		// 如果 GPU 已完成该分配器的命令
 		if (allocatorPair.first <= completedFenceValue)
