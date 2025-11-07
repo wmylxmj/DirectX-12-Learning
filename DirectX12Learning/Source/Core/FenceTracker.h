ebb5b300c371cbb5275f84d876e3fa3d354f6c64
wmylxmj <1397322329@qq.com>
2025-11-07 10:24:04 +0800

    更新围栏追踪器

--- DirectX12Learning/Source/Core/FenceTracker.h
+++ DirectX12Learning/Source/Core/FenceTracker.h
@@ -2,3 +2,23 @@
 
 #include "PrecompiledHeader.h"
 #include "CommandQueueManager.h"
+
+class FenceTracker
+{
+public:
+	FenceTracker(CommandQueueManager* commandQueueManager);
+	~FenceTracker();
+
+	void WaitForFence(UINT64 fenceValue);
+	void WaitForFence(UINT64 fenceValue, ID3D12CommandQueue* commandQueue);
+
+	UINT64 GetFenceValue() const;
+	UINT64 GetNextFenceValue() const;
+
+private:
+	void CreateFence();
+
+	CommandQueueManager* m_commandQueueManager;
+	ComPtr<ID3D12Fence> m_fence;
+	UINT64 m_fenceValue;
+};
