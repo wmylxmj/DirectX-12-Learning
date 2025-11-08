9da7f1ea450ea58916c7815f080fc4760ee135b6
wmylxmj <1397322329@qq.com>
2025-11-08 14:18:08 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.h
+++ DirectX12Learning/Source/Core/CommandQueue.h
@@ -17,7 +17,9 @@ public:
 	bool IsFenceValueCompleted(uint64_t fenceValue);
 	void StallForAnotherQueueFence(const CommandQueue& queue, uint64_t fenceValue);
 	void StallForAnotherQueueCompletion(const CommandQueue& queue);
-	void WaitForFence(uint64_t fenceValue);
+
+	void WaitForFenceValue(uint64_t fenceValue);
+
 	void WaitForIdle();
 
 	Microsoft::WRL::ComPtr<ID3D12CommandQueue> GetCommandQueue() const;
