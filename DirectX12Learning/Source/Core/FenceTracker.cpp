8df296397dc422eb9c4aa6b95aa113af6e0f1f10
wmylxmj <1397322329@qq.com>
2025-11-11 20:43:17 +0800

    更新围栏追踪器

--- DirectX12Learning/Source/Core/FenceTracker.cpp
+++ DirectX12Learning/Source/Core/FenceTracker.cpp
@@ -4,3 +4,7 @@ FenceTracker::FenceTracker(CommandQueueManager& commandQueueManager)
 	: m_commandQueueManager(commandQueueManager)
 {
 }
+
+void FenceTracker::SetPendingFenceValue(D3D12_COMMAND_LIST_TYPE commandListType, uint64_t fenceValue)
+{
+}
