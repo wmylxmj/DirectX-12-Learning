f31fdf6ab5df39e12f604bea91bf538293bdaa64
wmylxmj <1397322329@qq.com>
2025-11-13 19:50:58 +0800

    更新对齐

--- DirectX12Learning/Source/Math/Align.h
+++ DirectX12Learning/Source/Math/Align.h
@@ -1,1 +1,13 @@
 #pragma once
+
+template<typename T>
+inline T AlignUp(T size, size_t alignment) noexcept
+{
+	if (alignment > 0)
+	{
+		assert(((alignment - 1) & alignment) == 0);
+		auto mask = static_cast<T>(alignment - 1);
+		return (size + mask) & ~mask;
+	}
+	return size;
+}
