0dcd7a0a60ae2dfcf711a7bb16971bab6706cbf0
wmylxmj <1397322329@qq.com>
2025-11-23 13:50:26 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.cpp
+++ DirectX12Learning/Source/Core/CommandQueue.cpp
@@ -1,22 +1,5 @@
 ﻿#include "CommandQueue.h"
 
-m_pDevice(pDevice),
-m_kCommandListType(commandListType),
-m_fenceValue(0),
-m_completedFenceValueCache(0),
-m_commandAllocatorPool(pDevice, commandListType),
-m_commandListPool(pDevice, commandListType)
-{
-	// 创建命令队列
-	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-	queueDesc.Type = commandListType;
-	queueDesc.NodeMask = 1;
-	CHECK_HRESULT(pDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_pCommandQueue)));
-
-	// 创建围栏
-	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
-}
-
 uint64_t CommandQueue::IncrementFenceValue()
 {
 	std::lock_guard<std::mutex> lockGuard(m_fenceMutex);
