4e0584131335216d90d321ac3f1ef77edcb0694c
wmylxmj <1397322329@qq.com>
2025-11-23 14:02:50 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.cpp
+++ DirectX12Learning/Source/Core/CommandQueue.cpp
@@ -1,4 +1,27 @@
-﻿#include "CommandQueue.h"
+﻿#include "CommandQueue.h"﻿#include "CommandQueue.h"
+
+std::atomic_uint64_t CommandQueue::sm_nextNonReusableId = 1;
+
+CommandQueue::CommandQueue(ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE commandListType) :
+	m_kCommandListType(commandListType),
+	m_kNonReusableId(sm_nextNonReusableId++),
+	m_pDevice(pDevice),
+	m_fenceValue(0),
+	m_completedFenceValueCache(0),
+	m_commandAllocatorPool(pDevice, commandListType),
+	m_commandListPool(pDevice, commandListType)
+{
+	// 创建命令队列
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Type = commandListType;
+	queueDesc.NodeMask = 1;
+	CHECK_HRESULT(pDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_pCommandQueue)));
+
+	// 创建围栏
+	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
+}
+
+u
 
 uint64_t CommandQueue::IncrementFenceValue()
 {
