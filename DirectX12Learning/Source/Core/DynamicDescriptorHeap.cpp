124e9c6f69128b3a0eab60cf9f15825d6cbe501a
wmylxmj <1397322329@qq.com>
2025-11-27 15:08:27 +0800

    更新动态描述符堆

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
@@ -53,19 +53,3 @@ void DescriptorHeapManager::DiscardDescriptorHeaps(std::vector<DescriptorHeap*>&
 		m_retiredDescriptorHeaps.push(descriptorHeap);
 	}
 }
-
-void DescriptorHeapManager::RecordDescriptorHeapsFence(Microsoft::WRL::ComPtr<ID3D12Device> pDevice, const CommandQueue& commandQueue, const std::vector<DescriptorHeap*>& descriptorHeaps)
-{
-	std::lock_guard<std::mutex> lock(m_mutex);
-
-	if (!m_fenceMap.contains(commandQueue.GetNonReusableId())) {
-		m_fenceMap.emplace(commandQueue.GetNonReusableId(), std::make_unique<Fence>(pDevice));
-	}
-
-	m_fenceMap[commandQueue.GetNonReusableId()]->IncrementFenceValue(commandQueue.GetCommandQueue());
-
-	// 为每个描述符堆记录对应ID的围栏值
-	for (auto descriptorHeap : descriptorHeaps) {
-		descriptorHeap->m_pendingFences[commandQueue.GetNonReusableId()] = m_fenceMap[commandQueue.GetNonReusableId()]->GetFenceValue();
-	}
-}
