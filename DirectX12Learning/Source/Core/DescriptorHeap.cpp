36785f66ac2d1d654cf0e6e2e8e459595d6deca8
wmylxmj <1397322329@qq.com>
2025-11-28 14:07:31 +0800

    更新描述符堆

--- DirectX12Learning/Source/Core/DescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DescriptorHeap.cpp
@@ -9,8 +9,8 @@ DescriptorHeap::DescriptorHeap(ID3D12Device* pDevice, D3D12_DESCRIPTOR_HEAP_TYPE
 	D3D12_DESCRIPTOR_HEAP_DESC descriptorHeapDesc = {};
 	descriptorHeapDesc.Type = descriptorHeapType;
 	descriptorHeapDesc.NumDescriptors = numDescriptors;
-	descriptorHeapDesc.Flags
-		descriptorHeapDesc.NodeMask = 1;
+	descriptorHeapDesc.Flags = descriptorHeapFlags;
+	descriptorHeapDesc.NodeMask = 1;
 
 	CHECK_HRESULT(pDevice->CreateDescriptorHeap(&descriptorHeapDesc, IID_PPV_ARGS(&m_pDescriptorHeap)));
 
