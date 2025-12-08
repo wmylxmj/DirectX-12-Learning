ca0efe605b8db7b68440794ec03593fbddce1571
wmylxmj <1397322329@qq.com>
2025-12-08 15:31:07 +0800

    更新设备

--- DirectX12Learning/Source/Core/Device.cpp
+++ DirectX12Learning/Source/Core/Device.cpp
@@ -7,4 +7,8 @@ Device::Device(IUnknown* pAdapter)
 		D3D_FEATURE_LEVEL_11_0,
 		IID_PPV_ARGS(&m_pDevice)
 	));
+
+	m_commandAllocatorPoolMap.emplace(D3D12_COMMAND_LIST_TYPE_DIRECT, std::make_unique<CommandAllocatorPool>(m_pDevice.Get()));
+	m_commandAllocatorPoolMap.emplace(D3D12_COMMAND_LIST_TYPE_COMPUTE, std::make_unique<CommandAllocatorPool>(m_pDevice.Get()));
+	m_commandAllocatorPoolMap.emplace(D3D12_COMMAND_LIST_TYPE_COPY, std::make_unique<CommandAllocatorPool>(m_pDevice.Get()));
 }
