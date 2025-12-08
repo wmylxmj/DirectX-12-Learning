c5c69f106ba12c3e4f199b3fe2c45ca5a2c28b28
wmylxmj <1397322329@qq.com>
2025-12-08 14:52:14 +0800

    GENGX

--- DirectX12Learning/Source/Core/CommandAllocatorPool.h
+++ DirectX12Learning/Source/Core/CommandAllocatorPool.h
@@ -10,7 +10,6 @@
 class CommandAllocatorPool
 {
 public:
-	CommandAllocatorPool(ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE commandListType);
 
 	ID3D12CommandAllocator* RequestCommandAllocator();
 
