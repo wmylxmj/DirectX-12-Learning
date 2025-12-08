d793ddb60435e27a7076b1cf7cee9095c8e8d191
wmylxmj <1397322329@qq.com>
2025-12-08 14:39:43 +0800

    更新

--- DirectX12Learning/Source/Core/CommandAllocatorPool.h
+++ DirectX12Learning/Source/Core/CommandAllocatorPool.h
@@ -11,6 +11,8 @@ class CommandAllocatorPool
 {
 public:
 
+	CommandAllocatorPool(ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE commandListType);
+
 	ID3D12CommandAllocator* RequestCommandAllocator(uint64_t completedFenceValue);
 
 private:
