44be204ae39080a14cf33838b4d2095d0191e1ea
wmylxmj <1397322329@qq.com>
2025-12-12 15:09:48 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueueManager.cpp
+++ DirectX12Learning/Source/Core/CommandQueueManager.cpp
@@ -17,6 +17,73 @@ CommandQueue::CommandQueue(uint64_t commandQueueId, ID3D12Device* pDevice, D3D12
 	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
 }
 
+uint64_t CommandQueue::IncrementFenceValue()
+{
+	std::lock_guard<std::mutex> lockGuard(m_fenceMutex);
+	m_fenceValue++;
+	CHECK_HRESULT(m_pCommandQueue->Signal(m_pFence.Get(), m_fenceValue));
+	return m_fenceValue;
+}
+
+bool CommandQueue::IsFenceValueCompleted(uint64_t fenceValue)
+{
+	// 避免查询过于频繁
+	if (fenceValue > m_completedFenceValueCache) {
+		m_completedFenceValueCache = std::max(m_completedFenceValueCache, m_pFence->GetCompletedValue());
+	}
+	return fenceValue <= m_completedFenceValueCache;
+}
+
+void CommandQueue::StallForAnotherQueueFence(const CommandQueue& queue, uint64_t fenceValue)
+{
+	m_pCommandQueue->Wait(queue.m_pFence.Get(), fenceValue);
+}
+
+void CommandQueue::StallForAnotherQueueCompletion(const CommandQueue& queue)
+{
+	m_pCommandQueue->Wait(queue.m_pFence.Get(), queue.m_fenceValue);
+}
+
+void CommandQueue::WaitForFenceValue(uint64_t fenceValue)
+{
+	if (IsFenceValueCompleted(fenceValue)) return;
+
+	m_pFence->SetEventOnCompletion(fenceValue, nullptr);
+	m_completedFenceValueCache = fenceValue;
+}
+
+void CommandQueue::WaitForIdle()
+{
+	WaitForFenceValue(IncrementFenceValue());
+}
+
+D3D12_COMMAND_LIST_TYPE CommandQueue::GetCommandListType() const
+{
+	return m_kCommandListType;
+}
+
+ID3D12CommandQueue* CommandQueue::GetCommandQueue() const
+{
+	return m_pCommandQueue.Get();
+}
+
+uint64_t CommandQueue::GetCurrentFenceValue() const
+{
+	return m_fenceValue;
+}
+
+uint64_t CommandQueue::ExecuteCommandList(ID3D12GraphicsCommandList* pCommandList)
+{
+	std::lock_guard<std::mutex> lockGuard(m_fenceMutex);
+
+	CHECK_HRESULT(pCommandList->Close());
+	m_pCommandQueue->ExecuteCommandLists(1, &RvalueToLvalue((ID3D12CommandList*)pCommandList));
+
+	m_fenceValue++;
+	CHECK_HRESULT(m_pCommandQueue->Signal(m_pFence.Get(), m_fenceValue));
+	return m_fenceValue;
+}
+
 // static 变量定义
 std::atomic_uint64_t CommandQueueManager::sm_nextCommandQueueId = 1;
 std::shared_mutex CommandQueueManager::sm_commandQueueMapMutex;
