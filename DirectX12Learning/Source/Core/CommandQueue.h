452594aa8fce3473ab79cc0ef5d6c1ff3de3b8a3
wmylxmj <1397322329@qq.com>
2025-12-26 20:04:01 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.h
+++ DirectX12Learning/Source/Core/CommandQueue.h
@@ -9,6 +9,27 @@ class CommandQueue
 public:
 	CommandQueue(ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE commandListType);
 
+	uint64_t IncrementFenceValue();
+
+	bool IsFenceValueCompleted(uint64_t fenceValue);
+
+	void StallForAnotherQueueFence(const CommandQueue& queue, uint64_t fenceValue);
+	void StallForAnotherQueueCompletion(const CommandQueue& queue);
+
+	void WaitForFenceValue(uint64_t fenceValue);
+	void WaitForIdle();
+
+	ID3D12CommandQueue* GetCommandQueue() const;
+	uint64_t GetCurrentFenceValue() const;
+
+	// 根据命令分配器创建命令列表
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CreateCommandList(ID3D12CommandAllocator* pCommandAllocator);
+
+	uint64_t ExecuteCommandList(ID3D12GraphicsCommandList* pCommandList);
+
+	uint64_t GetCommandQueueId() const;
+	D3D12_COMMAND_LIST_TYPE GetCommandListType() const;
+
 private:
 	Microsoft::WRL::ComPtr<ID3D12Device> m_pDevice;
 	const D3D12_COMMAND_LIST_TYPE m_kCommandListType;
