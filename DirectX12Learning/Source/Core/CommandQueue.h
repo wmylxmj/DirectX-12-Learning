687d5f66a6e9719212e0534da5ed9a704f7a0a26
wmylxmj <1397322329@qq.com>
2025-12-26 15:45:45 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.h
+++ DirectX12Learning/Source/Core/CommandQueue.h
@@ -4,3 +4,41 @@
 
 class CommandQueue
 {
+	friend class CommandQueueManager;
+public:
+	// 增加围栏值并返回新值
+	uint64_t IncrementFenceValue();
+
+	bool IsFenceValueCompleted(uint64_t fenceValue);
+
+	void StallForAnotherQueueFence(const CommandQueue& queue, uint64_t fenceValue);
+	void StallForAnotherQueueCompletion(const CommandQueue& queue);
+
+	void WaitForFenceValue(uint64_t fenceValue);
+	void WaitForIdle();
+
+	ID3D12CommandQueue* GetCommandQueue() const;
+	uint64_t GetCurrentFenceValue() const;
+
+	// 根据命令分配器创建命令列表
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CreateCommandList(ID3D12CommandAllocator* pCommandAllocator);
+
+	uint64_t ExecuteCommandList(ID3D12GraphicsCommandList* pCommandList);
+
+	uint64_t GetCommandQueueId() const;
+	D3D12_COMMAND_LIST_TYPE GetCommandListType() const;
+
+private:
+	CommandQueue(uint64_t commandQueueId, ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE commandListType);
+
+	const uint64_t m_kCommandQueueId;
+	Microsoft::WRL::ComPtr<ID3D12Device> m_pDevice;
+	const D3D12_COMMAND_LIST_TYPE m_kCommandListType;
+
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_pCommandQueue;
+
+	std::mutex m_fenceMutex;
+	Microsoft::WRL::ComPtr<ID3D12Fence> m_pFence;
+	uint64_t m_fenceValue;
+	uint64_t m_completedFenceValueCache;
+};
