104111f527bfabcea208b5a6e91bad6777a287fe
wmylxmj <1397322329@qq.com>
2025-12-27 14:11:59 +0800

    更新围栏追踪器

--- DirectX12Learning/Source/Core/FenceTracker.h
+++ DirectX12Learning/Source/Core/FenceTracker.h
@@ -10,12 +10,6 @@ class FenceTracker
 {
 public:
 	FenceTracker(Device& device);
-	: m_device(device)
-	{
-	}
-	uint64_t Signal(D3D12_COMMAND_LIST_TYPE type);
-	uint64_t GetCompletedFenceValue(D3D12_COMMAND_LIST_TYPE type) const;
-	uint64_t GetPendingFenceValue(D3D12_COMMAND_LIST_TYPE type) const;
 
 private:
 	Device& m_device;
