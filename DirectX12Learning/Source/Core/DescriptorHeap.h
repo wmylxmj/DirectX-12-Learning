e370782b6c574ea7350d197d5a39b2611d64439f
wmylxmj <1397322329@qq.com>
2025-12-27 13:27:20 +0800

    更新描述符堆

--- DirectX12Learning/Source/Core/DescriptorHeap.h
+++ DirectX12Learning/Source/Core/DescriptorHeap.h
@@ -4,6 +4,29 @@
 #include "DescriptorHandle.h"
 #include <unordered_map>
 
+#include <unordered_map>
+
+class DescriptorHandle
+{
+public:
+	DescriptorHandle();
+	DescriptorHandle(D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorHandle);
+	DescriptorHandle(D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorHandle, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorHandle);
+
+	void operator +=(int offsetScaledByDescriptorSize);
+	DescriptorHandle operator +(int offsetScaledByDescriptorSize) const;
+
+	operator D3D12_CPU_DESCRIPTOR_HANDLE() const;
+	operator D3D12_GPU_DESCRIPTOR_HANDLE() const;
+
+	bool IsShaderVisible() const;
+	bool IsNull() const;
+
+private:
+	D3D12_CPU_DESCRIPTOR_HANDLE m_cpuDescriptorHandle;
+	D3D12_GPU_DESCRIPTOR_HANDLE m_gpuDescriptorHandle;
+};
+
 class DescriptorHeap
 {
 public:
