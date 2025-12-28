a325472b7ad478f9424086f437997939127be1e9
wmylxmj <1397322329@qq.com>
2025-12-28 13:52:45 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.h
+++ DirectX12Learning/Source/Core/CommandQueue.h
@@ -31,4 +31,14 @@ public:
 	D3D12_COMMAND_LIST_TYPE GetCommandListType() const;
 
 private:
+	Microsoft::WRL::ComPtr<ID3D12Device> m_pDevice;
+	const D3D12_COMMAND_LIST_TYPE m_kCommandListType;
+
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_pCommandQueue;
+
+	std::mutex m_fenceMutex;
+	Microsoft::WRL::ComPtr<ID3D12Fence> m_pFence;
+	uint64_t m_fenceValue;
+	uint64_t m_completedFenceValueCache;
+};
 };
