bfb65f63231ca75de3e4cc85a5d2fbb2b128d5af
wmylxmj <1397322329@qq.com>
2026-01-01 20:17:52 +0800

    更新命令队列

--- DirectX12Learning/Source/Core/CommandQueue.cpp
+++ DirectX12Learning/Source/Core/CommandQueue.cpp
@@ -1,4 +1,4 @@
-#include "CommandQueue.h"
+﻿#include "CommandQueue.h"
 
 CommandQueue::CommandQueue(ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE commandListType) :
 	m_pDevice(pDevice),
@@ -6,4 +6,12 @@ CommandQueue::CommandQueue(ID3D12Device* pDevice, D3D12_COMMAND_LIST_TYPE comman
 	m_fenceValue(0),
 	m_completedFenceValueCache(0)
 {
+	// 눼쉔츱즈뚠죗
+	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+	queueDesc.Type = commandListType;
+	queueDesc.NodeMask = 1;
+	CHECK_HRESULT(pDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_pCommandQueue)));
+
+	// 눼쉔鍋으
+	CHECK_HRESULT(pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_pFence)));
 }
