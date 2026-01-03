bd591024d16f177911b4579da31d8d2f3d71a887
wmylxmj <1397322329@qq.com>
2026-01-03 13:04:36 +0800

    更新围栏追踪器

--- DirectX12Learning/Source/Core/FenceTracker.cpp
+++ DirectX12Learning/Source/Core/FenceTracker.cpp
@@ -4,3 +4,8 @@ FenceTracker::FenceTracker(Device& device) :
 	m_device(device)
 {
 }
+
+void FenceTracker::SetPendingFenceValue(D3D12_COMMAND_LIST_TYPE commandListType, uint64_t fenceValue)
+{
+	m_pendingFences[commandListType] = fenceValue;
+}
