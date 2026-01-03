f54b119082a55a782c9e698820a5e9cf933048d2
wmylxmj <1397322329@qq.com>
2026-01-03 13:02:45 +0800

    更新围栏追踪器

--- DirectX12Learning/Source/Core/FenceTracker.h
+++ DirectX12Learning/Source/Core/FenceTracker.h
@@ -11,6 +11,12 @@ class FenceTracker
 public:
 	FenceTracker(Device& device);
 
+	void SetPendingFenceValue(D3D12_COMMAND_LIST_TYPE commandListType, uint64_t fenceValue);
+
+	bool ArePendingFencesCompleted();
+
+private:
+
 private:
 	Device& m_device;
 	std::unordered_map<D3D12_COMMAND_LIST_TYPE, uint64_t> m_pendingFences;
