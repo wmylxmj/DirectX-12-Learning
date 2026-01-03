7a4914bfccd469b7f77a54e156c58b3a1a67bd01
wmylxmj <1397322329@qq.com>
2026-01-03 13:14:45 +0800

    更新围栏追踪器

--- DirectX12Learning/Source/Core/FenceTracker.cpp
+++ DirectX12Learning/Source/Core/FenceTracker.cpp
@@ -1,5 +1,7 @@
 #include "FenceTracker.h"
 
+#include "Device.h"
+
 FenceTracker::FenceTracker(Device& device) :
 	m_device(device)
 {
@@ -8,4 +10,16 @@ FenceTracker::FenceTracker(Device& device) :
 void FenceTracker::SetPendingFenceValue(D3D12_COMMAND_LIST_TYPE commandListType, uint64_t fenceValue)
 {
 	m_pendingFences[commandListType] = fenceValue;
+}
+
+bool FenceTracker::ArePendingFencesCompleted()
+{
+	for (const auto& [commandListType, pendingFenceValue] : m_pendingFences)
+	{
+		if (!m_device.GetCommandQueue(commandListType).IsFenceValueCompleted(pendingFenceValue))
+		{
+			return false;
+		}
+	}
+	return true;
 }
