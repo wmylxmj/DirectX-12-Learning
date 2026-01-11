71f85301a6a8ad5cb2938202cf0da25f44607235
wmylxmj <1397322329@qq.com>
2026-01-11 19:55:25 +0800

    更新

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
@@ -3,6 +3,8 @@
 #include "Device.h"
 #include "RootSignature.h"
 
+#include <intrin.h>
+
 DescriptorHeapManager::DescriptorHeapManager(ID3D12Device* pDevice, D3D12_DESCRIPTOR_HEAP_TYPE descriptorHeapType, D3D12_DESCRIPTOR_HEAP_FLAGS descriptorHeapFlags, uint32_t generalDescriptorHeapSize) :
 	m_pDevice(pDevice),
 	m_kDescriptorHeapType(descriptorHeapType),
