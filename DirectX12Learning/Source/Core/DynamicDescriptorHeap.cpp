1c821e563e45de38445ac1041a2f426ca8a5c7cb
wmylxmj <1397322329@qq.com>
2026-01-13 18:51:42 +0800

    更新

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
@@ -170,6 +170,7 @@ uint32_t DynamicDescriptorHeap::DescriptorHandleCache::ComputeStagedSize()
 
 	while (_BitScanForward64(&rootParameterIndex, staleTableParameters))
 	{
+		staleTableParameters ^= (static_cast<uint64_t>(1) << rootParameterIndex);
 	}
 }
 
