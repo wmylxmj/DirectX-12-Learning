1ef4d62f2f5049f0b1c8e334ec2d85e6137cf712
wmylxmj <1397322329@qq.com>
2026-01-15 19:50:39 +0800

    GX

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
@@ -164,8 +164,6 @@ void DynamicDescriptorHeap::DescriptorHandleCache::StageDescriptorHandles(uint32
 
 void DynamicDescriptorHeap::DescriptorHandleCache::CopyAndBindStaleDescriptorTables(D3D12_DESCRIPTOR_HEAP_TYPE descriptorHeapType, DescriptorHandle baseDestinationDescriptorHandle, uint32_t descriptorSize, ID3D12GraphicsCommandList* pCommandList, void(__stdcall ID3D12GraphicsCommandList::* pSetDescriptorHeap)(UINT, D3D12_GPU_DESCRIPTOR_HANDLE))
 {
-	UINT numDestDescriptorRanges = 0;
-
 	uint64_t staleTableParameters = m_staleRootDescriptorTablesBitMap;
 	unsigned long rootParameterIndex;
 
@@ -176,6 +174,8 @@ void DynamicDescriptorHeap::DescriptorHandleCache::CopyAndBindStaleDescriptorTab
 		DescriptorTableEntry& descriptorTableEntry = m_rootDescriptorTables[rootParameterIndex];
 
 		const auto& markerRanges = descriptorTableEntry.assignedDescriptorHandlesMarker.GetMarkerRanges();
+
+		UINT numDestDescriptorRanges = markerRanges.size();
 
 		std::unique_ptr<D3D12_CPU_DESCRIPTOR_HANDLE[]> pDestDescriptorRangeStarts = std::make_unique<D3D12_CPU_DESCRIPTOR_HANDLE[]>(markerRanges.size());
 	}
