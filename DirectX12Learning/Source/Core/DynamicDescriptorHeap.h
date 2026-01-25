68555a0d093dadd80aeb04ac91e5c8604be34c9f
wmylxmj <1397322329@qq.com>
2026-01-25 20:06:16 +0800

    gx

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.h
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.h
@@ -110,7 +110,6 @@ private:
 
 		void CopyAndBindStaleDescriptorTables(ID3D12Device* pDevice, D3D12_DESCRIPTOR_HEAP_TYPE descriptorHeapType, DescriptorHandle baseDestinationDescriptorHandle, uint32_t descriptorSize, ID3D12GraphicsCommandList* pCommandList, void(__stdcall ID3D12GraphicsCommandList::* pSetDescriptorHeap)(UINT, D3D12_GPU_DESCRIPTOR_HANDLE));
 		uint32_t ComputeStagedSize();
-		void UnbindAllValid();
 
 		uint32_t ComputeCommittedSize();
 
