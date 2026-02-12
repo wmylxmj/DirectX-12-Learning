ffebd2930b6fb27691b88a054102f2b14fd88206
wmylxmj <1397322329@qq.com>
2026-02-12 15:20:13 +0800

    GX

--- DirectX12Learning/Source/Core/CommandContext.cpp
+++ DirectX12Learning/Source/Core/CommandContext.cpp
@@ -1,5 +1,7 @@
 #include "CommandContext.h"
 
-CommandContext::CommandContext(Device& device, D3D12_COMMAND_LIST_TYPE commandListType)
+CommandContext::CommandContext(Device& device, D3D12_COMMAND_LIST_TYPE commandListType) :
+	m_dynamicCbvSrvUavDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV),
+	m_dynamicSamplerDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER),
 {
 }
