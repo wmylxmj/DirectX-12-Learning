bc36e812090cd42d381dcc56513efa8a02c9888d
wmylxmj <1397322329@qq.com>
2026-02-12 15:37:30 +0800

    gx

--- DirectX12Learning/Source/Core/CommandContext.h
+++ DirectX12Learning/Source/Core/CommandContext.h
@@ -12,16 +12,16 @@ public:
 	CommandContext(Device& device, D3D12_COMMAND_LIST_TYPE commandListType);
 
 private:
-	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
-	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_pCommandList;
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_pCommandAllocator;
 
-	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_currentGraphicsRootSignature;
-	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_currentComputeRootSignature;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_pCurrentGraphicsRootSignature;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_pCurrentComputeRootSignature;
 
 	DynamicDescriptorHeap m_dynamicCbvSrvUavDescriptorHeap;
 	DynamicDescriptorHeap m_dynamicSamplerDescriptorHeap;
 
-	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_currentDescriptorHeaps[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES];
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_pCurrentDescriptorHeaps[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES];
 
 	LinearAllocator m_uploadHeapLinearAllocator;
 };
