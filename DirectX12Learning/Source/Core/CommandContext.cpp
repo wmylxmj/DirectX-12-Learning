28752669465abc7c86f917b956e57ac0dceb3728
wmylxmj <1397322329@qq.com>
2026-02-12 15:26:28 +0800

    gx

--- DirectX12Learning/Source/Core/CommandContext.cpp
+++ DirectX12Learning/Source/Core/CommandContext.cpp
@@ -2,6 +2,8 @@
 
 CommandContext::CommandContext(Device& device, D3D12_COMMAND_LIST_TYPE commandListType) :
 	m_dynamicCbvSrvUavDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV),
-	m_dynamicSamplerDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER)
+	m_dynamicSamplerDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER),
+	m_uploadHeapLinearAllocator(device, D3D12_HEAP_TYPE_UPLOAD)
+
 {
 }
