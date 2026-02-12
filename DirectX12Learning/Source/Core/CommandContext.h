de9bc86e831a84d1ad0a728c0d7c5083c555b9a6
wmylxmj <1397322329@qq.com>
2026-02-12 14:14:50 +0800

    更新

--- DirectX12Learning/Source/Core/CommandContext.h
+++ DirectX12Learning/Source/Core/CommandContext.h
@@ -9,6 +9,4 @@ public:
 	CommandContext(Device& device, D3D12_COMMAND_LIST_TYPE commandListType);
 
 private:
-	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
-	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
 };
