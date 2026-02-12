643e4c618bc4ed72c9fc642ab57e9a8e502d4222
wmylxmj <1397322329@qq.com>
2026-02-12 14:46:38 +0800

    GENGX

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.cpp
@@ -97,6 +97,10 @@ void DynamicDescriptorHeap::SetGraphicsDescriptorHandles(uint32_t rootParameterI
 	m_graphicsDescriptorHandleCache.StageDescriptorHandles(rootParameterIndex, offset, numDescriptors, descriptorHandles);
 }
 
+void DynamicDescriptorHeap::SetComputeDescriptorHandles(uint32_t rootParameterIndex, uint32_t offset, uint32_t numDescriptors, const D3D12_CPU_DESCRIPTOR_HANDLE* descriptorHandles)
+{
+}
+
 void DynamicDescriptorHeap::AssignedDescriptorHandlesMarker::MarkRange(uint32_t beginOffset, uint32_t endOffset)
 {
 	MarkerRange newMarkerRange;
