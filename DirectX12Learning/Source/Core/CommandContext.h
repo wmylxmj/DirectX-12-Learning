343b6629ccf617a86be6e3ad33ffcc9fdb15fda9
wmylxmj <1397322329@qq.com>
2026-02-12 15:03:38 +0800

    GX

--- DirectX12Learning/Source/Core/CommandContext.h
+++ DirectX12Learning/Source/Core/CommandContext.h
@@ -12,5 +12,7 @@ private:
 	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
 	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
 
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_currentGraphicsRootSignature;
+
 	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_currentDescriptorHeaps[D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES];
 };
