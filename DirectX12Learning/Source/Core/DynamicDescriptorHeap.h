d8fd7b8186872970ce56c9990a53a4802527f847
wmylxmj <1397322329@qq.com>
2026-02-25 19:27:50 +0800

    Revert "gx"
    This reverts commit 62b32c3720837c3659664c8eeb33ea562a13423a.

--- DirectX12Learning/Source/Core/DynamicDescriptorHeap.h
+++ DirectX12Learning/Source/Core/DynamicDescriptorHeap.h
@@ -60,9 +60,6 @@ public:
 	void CommitGraphicsRootDescriptorTables(CommandContext& commandContext);
 
 private:
-
-	void RetireCurrentHeap();
-
 	Device& m_device;
 	const D3D12_DESCRIPTOR_HEAP_TYPE m_kDescriptorHeapType;
 	DescriptorHeapManager* m_pDescriptorHeapManager;
