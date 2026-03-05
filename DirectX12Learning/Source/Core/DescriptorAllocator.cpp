a6e5f60528a062511d6b742e7a284ad5b003788a
wmylxmj <1397322329@qq.com>
2026-03-05 15:52:02 +0800

    GX

--- DirectX12Learning/Source/Core/DescriptorAllocator.cpp
+++ DirectX12Learning/Source/Core/DescriptorAllocator.cpp
@@ -1,1 +1,9 @@
 #include "DescriptorAllocator.h"
+
+DescriptorAllocator::DescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE descriptorHeapType) :
+	m_kDescriptorHeapType(descriptorHeapType),
+	m_pCurrentDescriptorHeap(nullptr),
+	m_currentDescriptorHeapOffset(0)
+
+{
+}
